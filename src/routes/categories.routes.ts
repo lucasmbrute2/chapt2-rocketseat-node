8ed5af4d2388db1b9fa9600513b790a1e45e07ad
Lucas <lucasmbrute614@gmail.com>
2022-05-12 00:12:53 -0300

    feat: add middleware on server

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const routes = Router();
 
 const categories = [];
 
-routes.post("/categories", (req, res) => {
+routes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  categories.push({
+  const category = {
+    id: uuid(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return res.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { routes } from "./routes/categories.routes";
 const port = 3333;
 const app = express();
 app.use(express.json());
-app.use(routes);
+app.use("/categories", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
