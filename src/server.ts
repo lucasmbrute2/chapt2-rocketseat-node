1de9ea5abcb1b504c053de8f7cd3ac846400a53e
Lucas <lucasmbrute614@gmail.com>
2022-05-12 00:02:46 -0300

    feat: add routes as a middleware on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { routes } from "./routes/categories.routes";
 
 const port = 3333;
 const app = express();
-
+app.use(routes);
 app.use(express.json());
 
 app.listen(port, () => {
