33ad9e70c24279ee72ffa4e82291a8196810e1e7
Lucas <lucasmbrute614@gmail.com>
2022-05-11 23:38:53 -0300

    feat: script dev & removing dist folder

--- dist/createCourseService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateCourseService {
-    execute(name, duration, instructor) {
-        console.log(` Olá ${name}, sua ${duration} é ${instructor}`);
-    }
-}
-exports.default = new CreateCourseService;
