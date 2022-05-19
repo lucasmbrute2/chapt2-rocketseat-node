dd591f38ce72bbe774c57205e05d83e7cedf86c4
Lucas <lucasmbrute614@gmail.com>
2022-05-19 00:01:45 -0300

    refactor: replacing routes from server to separate file index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
 import express from "express";
-import { routes } from "./routes/categories.routes";
-import { specificationRoutes } from "./routes/specifications.routes";
 
 const port = 3333;
 const app = express();
 app.use(express.json());
-app.use("/categories", routes);
-app.use("/specifications", specificationRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
