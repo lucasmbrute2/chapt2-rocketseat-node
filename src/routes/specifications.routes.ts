d89f2e8a46edacf229b0feb1a5d5907332ac833b
Lucas <lucasmbrute614@gmail.com>
2022-05-18 23:59:31 -0300

    refactor: Removing all bussniss logic from route

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,25 +1,10 @@
 import { Router } from "express";
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
 
-const specificationRepository = new SpecificationRepository();
-
 specificationRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const specificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  try {
-    specificationService.execute({ description, name });
-  } catch (error) {
-    res.status(404).json({ error });
-  }
-
-  return res.status(201).send();
+  createSpecificationController.handle(req, res);
 });
 
 export { specificationRoutes };
