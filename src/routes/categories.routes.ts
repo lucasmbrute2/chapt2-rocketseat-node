4c2f95b6c190cfc187f31f766bb80f8b23138dec
Lucas <lucasmbrute614@gmail.com>
2022-05-19 00:44:42 -0300

    feat: add multer to controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRouter = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRouter.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -10,6 +15,10 @@ categoriesRouter.post("/", (req, res) => {
 
 categoriesRouter.get("/", (req, res) => {
   listCategoriesController.handle(req, res);
+});
+
+categoriesRouter.post("/import", upload.single("file"), (req, res) => {
+  importCategoryController.handle(req, res);
 });
 
 export { categoriesRouter };
