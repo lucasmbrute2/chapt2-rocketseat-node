9c10a528d4d085a206a28f157df8982d73c69915
Lucas <lucasmbrute614@gmail.com>
2022-05-19 22:49:20 -0300

    feat: implemented stream to read file

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,11 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+export class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+  }
+}
