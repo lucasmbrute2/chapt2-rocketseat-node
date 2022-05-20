ebd45b647c91577b02804d26d1847a254a80638c
Lucas <lucasmbrute614@gmail.com>
2022-05-19 23:53:14 -0300

    feat: added swagger middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const port = 3333;
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
