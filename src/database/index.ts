dd0ba9390eec9ca44a182016202637f4be9bc170
Lucas Dantas <lucasmbrute614@gmail.com>
2022-06-09 23:20:31 -0300

    feat: added function to create connection with TypeORM

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,8 +2,13 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+  port: 5432,
   host: "localhost",
-  username: "test",
-  password: "test",
+  username: "docker",
+  password: "ignite",
   database: "rentx"
 })
+
+export function createConnection(host = "database"): Promise<DataSource> {
+  return AppDataSource.setOptions({ host }).initialize();
+}
