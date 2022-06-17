b8af0441b8bb965c3d9e66d767ce11a9702ddc4e
Lucas Dantas <lucasmbrute614@gmail.com>
2022-06-16 22:42:32 -0300

    refactor: creating entity category instead only push to array

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -2,15 +2,20 @@ import { ICreateCategoryDTO } from "../../dto/CategoriesDto";
 import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../ICategoriesRepository";
 
+import { Repository } from "typeorm";
+import AppDataSource from "../../../../database/data-source";
+
 // DTO=> Data transfer object
 // singleton pattern
 export class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[];
+
+  private repository: Repository<Category>
+
   // eslint-disable-next-line no-use-before-define
   private static INSTACE: CategoriesRepository;
 
   private constructor() {
-    this.categories = [];
+    this.repository = AppDataSource.getRepository(Category)
   }
 
   public static getInstace(): CategoriesRepository {
@@ -21,14 +26,13 @@ export class CategoriesRepository implements ICategoriesRepository {
     return CategoriesRepository.INSTACE;
   }
 
-  create({ name, description }: ICreateCategoryDTO): void {
-    const category = new Category();
-    Object.assign(category, {
-      name,
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
       description,
-    });
+      name
+    })
 
-    this.categories.push(category);
+    await this.repository.save(category);
   }
 
   list(): Category[] {
