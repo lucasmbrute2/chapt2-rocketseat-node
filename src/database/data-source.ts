70bedfec03ad8fea2e629309ed40f59214b4265e
Lucas Dantas <lucasmbrute614@gmail.com>
2022-06-19 23:11:50 -0300

    refactor: AppDataSource

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from "typeorm";
 
-const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource({
   type: "postgres",
   port: 5432,
   host: "database_ignite",
@@ -8,11 +8,12 @@ const AppDataSource = new DataSource({
   password: "ignite",
   database: "rentx",
   migrationsRun: true,
-  migrations: [__dirname + "/migrations/**/*{.js,.ts}"]
+  entities: ["Category"],
+  migrations: ["1654826967781-CreateCategories"]
 })
 
-export async function createConnection(): Promise<DataSource> {
-  return await AppDataSource.initialize();
+export function createConnection(): void {
+  AppDataSource.initialize().then(() => {
+    console.log("DB connected")
+  }).catch(err => console.log(err));
 }
-
-export default AppDataSource
