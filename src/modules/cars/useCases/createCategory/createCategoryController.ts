d0a91a0488b5bfdd49725dfc1b1e303e388fc137
Lucas Dantas <lucasmbrute614@gmail.com>
2022-06-21 23:42:09 -0300

    refactor: refactrored controller to typeorm methods

--- src/modules/cars/useCases/createCategory/createCategoryController.ts
+++ src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 export class CreateCategoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) { }
 
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
     try {
-      this.createCategoryUseCase.execute({ description, name });
+      await this.createCategoryUseCase.execute({ name, description });
     } catch (e) {
       response.status(500).send({ error: e.message });
     }
