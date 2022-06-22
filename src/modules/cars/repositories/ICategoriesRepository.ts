6e0497ee7fdee538b03c66482b82d2189e02d522
Lucas Dantas <lucasmbrute614@gmail.com>
2022-06-21 23:58:21 -0300

    Revert "Revert "refactor: refactrored repository yo typeorm methods""
    This reverts commit f490a52401aa1ce51143bf7ae4ac455839931af1.

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -4,5 +4,5 @@ import { Category } from "../entities/Category";
 export interface ICategoriesRepository {
   findByName: (name: string) => Promise<Category>;
   list: () => Promise<Category[]>
-  create({ description, name }: ICreateCategoryDTO): Promise<void>;
+  createCategory({ description, name }: ICreateCategoryDTO): Promise<void>;
 }
